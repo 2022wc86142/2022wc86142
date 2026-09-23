85502d72a1d45906c47e01e606ff78907d8590c4
2022wc86142 <2022wc86142@wilp.bits-pilani.ac.in>
2026-09-23 19:20:13 +0530

    Updated source code to test Jenkins Poll SCM

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -2,6 +2,6 @@ package com.bits;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Continuous Integration through Jenkins completed successfully.");
+        System.out.println("Continuous Integration through Jenkins Poll SCM completed successfully.");
     }
 }
